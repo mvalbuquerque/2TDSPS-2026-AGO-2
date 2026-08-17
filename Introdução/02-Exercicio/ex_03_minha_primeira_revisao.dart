2ed0a1b62f7c4eaab8a3f3a81657c5118712d737
@mvalbuquerque <mvalbuquerque@users.noreply.github.com>
2026-08-17 20:55:41 -0300

    update

---
+++ Introdução/02-Exercicio/ex_03_minha_primeira_revisao.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  print('o que dart imprime?');
+
+  print('');
+
+  print('Dart é a linguagem usada para criar aplicativos com flutter');
+
+  print('');
+
+  print('Dart é a linguagem usada para criar aplicativos com flutter');
+
+}
---
+++ Introdução/02-Exercicio/exercio02_roteiro_console.dart
@@ -0,0 +1,18 @@
+
+void main() {
+
+  //Exibe o titulo da aula como cabeçalho visual com delimitadores "====="
+  print('===== Aula de Dart ========');
+
+
+  //os intens abaixo foram uma lista numerada de objetivos da aula. 
+  print('1. Aprender a sintaxe básica do Dart');  
+  print('2. Aprender a sintaxe básica do Flutter'); 
+  print('3. Praticar com prints');
+
+    print('====================');
+
+  //Indica o andamento atual do roteiro
+  print('Andamento do roteiro: 2/5');
+
+}
