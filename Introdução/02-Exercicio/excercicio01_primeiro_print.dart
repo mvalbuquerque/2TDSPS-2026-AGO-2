8253553b848af69cffed50709165c7cecd67fbd8
@mvalbuquerque <mvalbuquerque@users.noreply.github.com>
2026-08-17 20:43:53 -0300

    update

---
+++ Introdução/02-Exercicio/excercicio01_primeiro_print.dart
@@ -0,0 +1,15 @@
+//Este programa demostra como exibir informações pessoais no console
+void main() {
+
+  //Imprimindo informações pessoais no console
+  print("Nome: Vinny Albuquerque");
+
+  //Imprimindo informações de cidade de origem 
+  print("Cidade de origem: Ribeirao Pires - SP");
+
+  // Imprimindo informações do curso que você esta aprendendo, aqui a ideia
+  print('Objetivo: apreender Dart e Flutter');
+
+  // ESTA PASSANDO AQUI! 
+  print('esta passando aqui.. deu certo, passou, funcionoa coloca o nome da função');
+}
