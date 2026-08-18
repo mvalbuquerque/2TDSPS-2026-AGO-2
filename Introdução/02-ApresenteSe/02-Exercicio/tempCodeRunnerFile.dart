df2c07160d5de9da60047cb488be73a4c959d5d5
@mvalbuquerque <mvalbuquerque@users.noreply.github.com>
2026-08-17 23:04:52 -0300

    update

---
+++ Introdução/02-ApresenteSe/02-Exercicio/tempCodeRunnerFile.dart
@@ -0,0 +1,7 @@
+// A função main() é o ponto de entrada de todo programa Dart.
+// Quando o programa é executado, o Dart começa rodando o código aqui dentro.
+void main() {
+  // A função print() exibe uma mensagem de texto no console.
+  // O texto entre aspas simples é chamado de String (cadeia de caracteres).
+  print('Ola, Dart!');
+}
