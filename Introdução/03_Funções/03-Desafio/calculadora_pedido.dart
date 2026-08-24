a874db15b3ccc11b583813fa06c941da424ca7b1
@mvalbuquerque <mvalbuquerque@users.noreply.github.com>
2026-08-24 20:57:38 -0300

    update

---
+++ Introdução/03_Funções/03-Desafio/calculadora_pedido.dart
@@ -0,0 +1,17 @@
+//Calculadora de pedidos com três funções encadeadas, 
+//cada uma com responsabilidade única: calcular o subtotal, calcular o valor do desconto e
+//calcular o total final — o resultado de uma alimenta a próxima.
+
+//Terminou envia no o link do seu github aqui no chat....
+
+//Console 
+ 
+ /*
+
+=== Pedido ===
+Produto: Mochila
+Subtotal: R$ 240.00
+Desconto: R$ 36.00
+Total: R$ 204.00
+
+*/
